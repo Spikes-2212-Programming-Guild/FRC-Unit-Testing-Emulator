c964e36784ef6a55067525cbf53e71b1dedfc928
ThinkRedstone <ittai11@gmail.com>
2015-01-17 16:48:27 +0200

    added reset() to Encoder

--- src/edu/wpi/first/wpilibj/Encoder.java
+++ src/edu/wpi/first/wpilibj/Encoder.java
@@ -10,15 +10,19 @@ package edu.wpi.first.wpilibj;
  * @author eyal
  */
 public class Encoder {
-        
-    public Encoder(int port1, int port2){
+
+    public Encoder(int port1, int port2) {
     }
-    
-    public double get(){
+
+    public double get() {
         return 0;
     }
 
     public void start() {
     }
-    
+
+    public void reset() {
+
+    }
+
 }
